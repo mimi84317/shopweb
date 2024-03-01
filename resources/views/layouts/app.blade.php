33bf21815beb2dd94aaefdcdcf258dc7b7e815e5
mimi84317 <mimi84317@gmail.com>
2024-03-02 03:17:58 +0800

    更新

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Laravel 10 Task List App</title>
+  <script src="https://cdn.tailwindcss.com"></script>
+  <style type="text/tailwindcss"></style>
+</head>
+
+<body>
+    <h1>@yield('title')</h1>
+    <div>
+        @yield('content')
+    </div>
+</body>
+
+</html>
