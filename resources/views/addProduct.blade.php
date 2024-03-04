fd89f1eb081459d00609a41fa819add7c337f7a6
mimi84317 <mimi84317@gmail.com>
2024-03-05 01:06:02 +0800

    新增新增商品頁面

--- resources/views/addProduct.blade.php
+++ resources/views/addProduct.blade.php
@@ -1,1 +1,33 @@
 @extends('layouts.app')
+
+@section('title', '新增商品')
+
+@section('content')
+<form method="POST" action="">
+    <div>
+        <label>商品名稱 :</label>
+        <input text="text" name="productName" id="productName"/>
+    </div>
+    <div>
+        <label>商品類別 :</label>
+        <select name="productType" id="productType">
+            <option value="">請選擇</option>
+            <option value="book">書籍</option>
+            <option value="stationery">文具</option>
+            <option value="dailyuse">日用品</option>
+            <option value="clothes">衣服</option>
+            <option value="3C">3C</option>
+            <option value="food">美食</option>
+        </select>
+        <input text="text" name="productName" id="productName"/>
+    </div>
+    <div>
+        <label>商品描述 :</label>
+        <textarea text="text" name="productDescription" id="productDescription" rows="5"></textarea>
+    </div>
+
+</form>
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+</h1>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,29 @@
 <!DOCTYPE html>
 <html>
 
-<head>
-  <title>Laravel 10 Task List App</title>
-  <script src="https://cdn.tailwindcss.com"></script>
-  <style type="text/tailwindcss"></style>
-</head>
+    <head>
+        <title>shopweb</title>
 
-<body>
-    <h1>@yield('title')</h1>
-    <div>
-        @yield('content')
-    </div>
-</body>
+        <meta charset="UTF-8">
 
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="//unpkg.com/alpinejs" defer></script>
+
+        <style type="text/tailwindcss">
+            @layer utilities {
+                .content-auto {
+                    content-visibility: auto;
+                }
+            }
+        </style>
+        @yield('styles')
+
+    </head>
+
+    <body>
+        <h1>@yield('title')</h1>
+        <div>
+            @yield('content')
+        </div>
+    </body>
 </html>
